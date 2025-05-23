b9c6f724f904ec78e9134d93762f973de86106db
Michael Forbes <m.b.forbes@outlook.com>
2025-05-23 12:49:38 +1000

    Added templates

--- db/migrate/20250521022708_initial_database.rb
+++ db/migrate/20250521022708_initial_database.rb
@@ -78,11 +78,11 @@ class InitialDatabase < ActiveRecord::Migration[8.0]
     t.integer "failed_attempts", default: 0, null: false
     t.datetime "locked_at"
     t.string "unlock_token"
+    t.boolean "active", default: true, null: false
     t.bigint "created_by", null: false
     t.bigint "updated_by", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "active", default: true, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
