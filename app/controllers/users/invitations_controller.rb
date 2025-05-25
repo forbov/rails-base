1ae1cc982a92460b9f8bcf7213b1da44f1ec9998
Michael Forbes <m.b.forbes@outlook.com>
2025-05-26 09:31:03 +1000

    Refined invite function

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -7,28 +7,28 @@ class Users::InvitationsController < Devise::InvitationsController
   end
 
   def create
-    user_params = params[:user]
-    user_role = Role.find_by(name: user_params[:user_role_id])
+    user_role = Role.find_by(name: invite_params[:user_role_id])
     notice = nil
-    email = invite_params[:email]
-    user_role_id = invite_params[:user_role_id]
 
-    existing_user = User.find_by(email:)
+    existing_user = User.find_by(email: invite_params[:email])
     user = nil
 
     if existing_user.present?
       user = existing_user
-      notice = "User with email #{email} already exists, just adding #{user_role.description} role to the user."
+      notice = "User with email #{user.email} already exists, just adding #{user_role.description} role to the user."
     else
-      user = User.invite!(invite_params,
-                          current_inviter,
-                          skip_invitation: true) do |u|
-        u.skip_invitation = true
-      end
-      notice = "Invitation sent to #{email}, roles and relationships added."
+      user = User.new(email: invite_params[:email],
+                      first_name: invite_params[:first_name],
+                      last_name: invite_params[:last_name],
+                      mobile_phone: invite_params[:mobile_phone],
+                      created_by: invite_params[:created_by],
+                      updated_by: invite_params[:updated_by])
+
+      notice = "Invitation sent to #{user.email}, roles and relationships added."
     end
-    user.add_role(user_role_id) unless user.has_role? user_role_id
+    user.add_role(user_role.name) unless user.has_role? user_role.name
     user.send_new_user_email(current_user)
+    user.add_role(user_role.name) unless user.has_role? user_role.name
     redirect_to users_path, notice:
   end
 
@@ -44,5 +44,12 @@ class Users::InvitationsController < Devise::InvitationsController
   def update_resource_params
     params.require(:user).permit(:password, :password_confirmation, :user, :email, :invitation_token, :updated_by)
   end
-  
+
+  # This is called when creating invitation.
+  # It should return an instance of resource class.
+  def invite_resource
+    # skip sending emails on invite
+    super { |user| user.skip_invitation = true }
+    # super
+  end
 end
